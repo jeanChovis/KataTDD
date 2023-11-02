d786d8a757a5ded42cc074bdebe9a9150a71e527
jean <76266001@continental.edu.pe>
2023-11-02 16:38:47 -0500

    Calcular con conjunto vacio (Rojo)

--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -1,5 +1,8 @@
 import unittest
 from src.Logica.Conjunto import Conjunto
 
+
 class TestConjunto( unittest.TestCase ):
-    pass
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
